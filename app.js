5172155e137f247ed94e90e480ab514f3e1d619e
Ashish Trivedi <triashish7@gmail.com>
2020-04-22 23:21:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const flash = require('connect-flash');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://Ashish:root@cluster0-ijuae.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI = '';
 
 const store = new MongoDBStore({
 	uri: MONGODB_URI,
